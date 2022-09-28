8226a17a36633a092a490e38f87c54cd60d76ea6
Edson Urano <e.uranoc@gmail.com>
2022-09-28 19:05:31 -0300

    Atualizando o projeto

--- README.md
+++ README.md
@@ -1,1 +1,180 @@
-# Tetris
+<br />
+<p align="center">
+  <a href="https://github.com/EdsonUr/Tetris">
+    <img src="Assets/logo.png" alt="Logo" width="100" height="70">
+  </a>
+
+  <h3 align="center">Tetris</h3>
+
+</p>
+
+
+<details open="open">
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#preview">Preview</a>
+    </li>
+    <li>
+      <a href="#sobre-o-projeto">Sobre o projeto</a>
+    </li>
+    <li>
+      <a href="#jogabilidade">Jogabilidade</a>
+    </li>
+    <li>
+      <a href="#screenshots">Screenshots</a>
+    </li>
+  </ol>
+</details>
+
+## Preview
+Vídeo de demonstração: <br>
+https://drive.google.com/file/d/1JD9SNCoiHMX3xcfIbgVts9UsaHQJb3Ze/view?usp=sharing
+
+## Sobre o projeto
+Este projeto foi um trabalho da disciplina de raciocínio lógico e algoritmico no primeiro semestre da faculdade de ciência da computação. <br>
+É uma versão do tetris, construída em vanilla javascript, html e css. <br>
+O jogo consiste em empilhar tetraminós que descem a tela de forma que completem 
+linhas horizontais. Quando uma linha se forma, ela se desintegra, as camadas superiores 
+descem, e o jogador ganha pontos. Quando a pilha de peças chega ao topo da tela, a partida se 
+encerra.
+
+
+## Jogabilidade
+<ul>
+    <li>
+    <h2>Mudança de nível</h2>
+        <ul>
+            <li>
+            O jogo suporta infinitos níveis, iniciando no nível 1.
+            </li>
+            <li>
+            A cada 10 linhas removidas, o jogador passa para o nível seguinte e a contagem de linhas removidas reinicia.
+            </li>
+            <li>
+            A cada nível, a velocidade do jogo aumenta.
+            </li>
+        </ul>
+    </li>
+<br/>
+    <li>
+        <h2>Contagem de pontos</h2>
+            <ul>
+                <li>
+                O jogador ganha pontos a cada linha que desaparecer.
+                </li>
+                <li>
+                A remoção de mais linhas de uma vez dá mais pontos que remover menos linhas:
+                <ul>
+                    <li>
+                    Uma linha: 100 pontos
+                    </li>
+                    <li>
+                    Duas linhas: 300 pontos
+                    </li>
+                    <li>
+                    Três linhas: 500 pontos
+                    </li>
+                    <li>
+                    Quatro linhas (Tetris): 800 pontos
+                    </li>
+                </ul>
+                </li>
+                <li>
+                A quantidade exata de pontos ganhos pela eliminação de linhas depende do nível em que se encontra o jogador(os pontos são multiplicados pelo nível).
+                </li>
+            </ul>
+        </li>
+<br>
+<li>
+        <h2>Teclas</h2>
+            <ul>
+                <li>
+                (S) -> desce o tetraminó 1 quadrado.
+                </li>
+                <li>
+                (A) -> movimenta a peça para a esquerda.
+                </li>
+                <li>
+                (D) -> movimenta a peça para a direita.
+                </li>
+                <li>
+                (Z) -> gira a peça para a esquerda.
+                </li>
+                <li>
+                (Espaço) -> queda forte.
+                </li>
+            </ul>
+        </li>
+<br>       
+<li>
+        <h2>Painéis</h2>
+            <ul>
+                <li>
+                Painel central -> contendo a tela do jogo
+                </li>
+                <li>
+                Painel esquerdo -> contendo a pontuação do jogador, o nível atual e a quantidade de linhas eliminadas.
+                </li>
+                <li>
+                Painel direito -> contendo os próximos 3 tetraminós.
+                </li>
+                <li>
+                Estatística -> No final do jogo aparece as quantidades de cada uma das peças utilizadas pelo jogador durante a partida.
+                </li>
+            </ul>
+        </li> 
+<br>
+<li>
+        <h2>Efeitos sonoros</h2>
+            <ul>
+                <li>
+                Música de fundo.
+                </li>
+                <li>
+                Movimento das peças para baixo e para os lados.
+                </li>
+                <li>
+                Rotação da peça.
+                </li>
+                <li>
+                Travamento da peça.
+                </li>
+                <li>
+                Eliminação de linhas.
+                </li>
+                <li>
+                Fim de jogo.
+                </li>
+            </ul>
+        </li>
+<br>
+<li>
+        <h2>Ranking</h2>
+            <ul>
+                <li>
+                No final da partida, o jogador deverá informar as iniciais do seu nome (máximo 3 letras).
+                </li>
+                <li>
+                Após informar suas iniciais, irá aparecer o rank com as 5 pontuações mais altas e as iniciais dos respectivos jogadores.
+                </li>
+                <li>
+                O ranking utiliza o algoritmo do bubble sort para ordenar e é mantido na aplicação usando persistência de dados.
+                </li>
+            </ul>
+        </li>
+</ul>
+<br>
+
+Fique à vontade para me contatar!
+
+## Screenshots
+ <a href="https://github.com/EdsonUr/Tetris">
+    <img src="Assets/ImgInicio.png" alt="Logo">
+ </a>
+ <a href="https://github.com/EdsonUr/Tetris">
+    <img src="Assets/ImgJogo.png" alt="Logo">
+ </a>
+  <a href="https://github.com/EdsonUr/Tetris">
+    <img src="Assets/ImgRank.png" alt="Logo">
+ </a>
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Vetor contendo todos os patterns da peça I:
 const I = [
 	[
 		[0, 0, 0, 0],
@@ -155,6 +156,17 @@ const Z = [
 	]
 ];
 
+var estatistica = {
+    I: 0,
+    J: 0,
+    L: 0,
+    O: 0,
+    S: 0,
+    T: 0,
+    Z: 0
+}
+
+//Possibilidades cor peças;
 var cores = ["pink","aqua","red","green","yellow","brown"]
 
 const PECAS = [
@@ -169,9 +181,17 @@ const PECAS = [
 
 const LINHA = 20; //qtd de linhas
 const COLUNA = 10; // qtd de colunas
-const TAMANHO = 30; //lado do quadrado em pxs
+const TAMANHO = 25; //lado do quadrado em pxs
 const VAGO = "black"; // cor de um quadrado sem peça
 
+var estatisticaa = document.getElementById("estatisticaa");
+var qtdZ = document.getElementById("qtdZ");
+var qtdS = document.getElementById("qtdS");
+var qtdT = document.getElementById("qtdT");
+var qtdO = document.getElementById("qtdO");
+var qtdL = document.getElementById("qtdL");
+var qtdI = document.getElementById("qtdI");
+var qtdJ = document.getElementById("qtdJ");
 var scoreCaixa = document.getElementById("score");
 var nivelCaixa = document.getElementById("nivelNumber");
 var linhasRemovidasCaixa = document.getElementById("linhasEliminadas");
@@ -191,11 +211,13 @@ var c = tela.getContext("2d");
 iniciarTabuleiro();
 
 desenharTabuleiro();
+var jogar = document.getElementById("jogar");
 
 function start(){
     playAudio("startGame");
 
-    document.getElementById("jogar").disabled=true;
+    jogar.classList.add("hidde");
+    jogar.disabled=true;
      
     onkeydown = controlarPeca;
     
@@ -213,6 +235,7 @@ function iniciarTabuleiro() {
 		tabuleiro[i] = [];
 		
 		for (var j = 0; j < COLUNA; j++) {
+            //todo quadrado = VAGO => black;
 			tabuleiro[i][j] = VAGO;
 		}
 	}
@@ -226,6 +249,7 @@ function desenharTabuleiro(){
     }
 }
 
+//x->número de quadrados da esquerda; y->número de quadrados do topo;
 function desenharQuadrado(x, y, cor){
     c.fillStyle = cor;
     c.fillRect(x*TAMANHO, y*TAMANHO, TAMANHO, TAMANHO);
@@ -234,21 +258,95 @@ function desenharQuadrado(x, y, cor){
     c.strokeRect(x*TAMANHO, y*TAMANHO, TAMANHO, TAMANHO);
 }
 
-function gerarPeca(){
-    var r = Math.floor(Math.random() * PECAS.length); // gerando um indice aleatorio para ser usado (de 0 -> 6).
-	
-	peca = {
-		tetramino : PECAS[r][0],
-		cor : cores[parseInt((Math.random()*5))],
-		tetraminoN : 0, // começa pelo primeiro pattern
-		tetraminoAtivo : [[]],
-		x : 3,
-		y : -2
-	};
-	
-	peca.tetraminoAtivo = peca.tetramino[peca.tetraminoN];
+var todasPecas = [];
+
+var next1 = document.getElementById("next1");
+var next2 = document.getElementById("next2");
+var next3 = document.getElementById("next3");
+
+function nextPiece(r,p){
+    if(todasPecas[r] == 0){
+        p.setAttribute('src', 'Z.png')
+    }
+    else if(todasPecas[r] == 1){
+        p.setAttribute('src', 'S.png')
+    }
+    else if(todasPecas[r] == 2){
+        p.setAttribute('src', 'T.png')
+    }
+    else if(todasPecas[r] == 3){
+        p.setAttribute('src', 'O.png')
+    }
+    else if(todasPecas[r] == 4){
+        p.setAttribute('src', 'L.png')
+    }
+    else if(todasPecas[r] == 5){
+        p.setAttribute('src', 'I.png')
+    }
+    else{
+        p.setAttribute('src', 'J.png')
+    }
 }
 
+function gerarPeca(){
+    if(todasPecas.length == 0 || todasPecas.length<3){
+        for(var i=0; i<4;i++){
+            var indice = Math.floor(Math.random() * PECAS.length); // gerando um indice aleatorio para ser usado (de 0 -> 6).
+            todasPecas.push(indice);
+        }
+    }
+    //PRÓXIMAS PEÇAS
+    nextPiece(1,next1);
+    nextPiece(2,next2);
+    nextPiece(3,next3);
+
+    //define as características da peça ativa.
+    var r = todasPecas.shift();
+        peca = {
+            tetramino : PECAS[r][0],
+            cor : cores[parseInt((Math.random()*5))],
+            tetraminoN : 0, // começa pelo primeiro pattern
+            tetraminoAtivo : [[]],
+            x : 3,
+            y : -2
+        };
+
+
+	peca.tetraminoAtivo = peca.tetramino[peca.tetraminoN];
+    //lógica parte estatística
+    if(r == 0){
+        estatistica.Z++;
+    }
+    else if(r == 1){
+        estatistica.S++;
+    }
+    else if(r == 2){
+        estatistica.T++;
+    }
+    else if(r == 3){
+        estatistica.O++;
+    }
+    else if(r == 4){
+        estatistica.L++;
+    }
+    else if(r == 5){
+        estatistica.I++;
+    }
+    else{
+        estatistica.J++;
+    }
+
+    //update na parte de estatística
+    qtdZ.innerHTML = estatistica.Z;
+    qtdS.innerHTML = estatistica.S;
+    qtdT.innerHTML = estatistica.T;
+    qtdO.innerHTML = estatistica.O;
+    qtdL.innerHTML = estatistica.L;
+    qtdI.innerHTML = estatistica.I;
+    qtdJ.innerHTML = estatistica.J;
+}
+
+var tempo = 1000;
 
 //controla a lógica de descida da peça
 function descerPeca(){
@@ -256,7 +354,7 @@ function descerPeca(){
     //diferença do tempo atual com a última vez que a peça desceu
     var delta = agora - inicioDescida;
 	//checar se já passou 1s(1000ms)
-    if (delta > 1380 - (nivel * 380)) {
+    if (delta > tempo) {
         moverAbaixo();
         inicioDescida = Date.now();
     }
@@ -264,6 +362,23 @@ function descerPeca(){
     if (!fimDeJogo) {
         requestAnimationFrame(descerPeca);
     }
+}
+
+function descerForte(){
+    if(!colisao(0,1,peca.tetraminoAtivo)){
+        apagarPeca();
+        while(!colisao(0,1,peca.tetraminoAtivo)){
+            peca.y++;
+            score += 2;
+            scoreCaixa.innerHTML = score;
+        }
+        desenharPeca();
+    }
+    else{
+        travarPeca();
+        gerarPeca();
+    }
+    moverAbaixo();
 }
 
 function moverAbaixo(){
@@ -295,6 +410,7 @@ function moverEsquerda(){
 }
 
 function colisao(x, y, p){
+    //passar por todo o tetromino ativo.
     for (var i = 0; i < p.length; i++) {
         for (var j = 0; j < p.length; j++) {
             //checar se o quadrado é vazio. Se for, continua.
@@ -337,6 +453,7 @@ function desenharPeca(){
 function preencherPeca(cor) {
     for (var i = 0; i < peca.tetraminoAtivo.length; i++) {
         for (var j = 0; j < peca.tetraminoAtivo.length; j++) {
+            //se a posição de uma peça estiver com 1 nessas coordenadas, desenhar! (linha, coluna).
             if (peca.tetraminoAtivo[i][j]) {
                 desenharQuadrado(peca.x + j, peca.y + i, cor);
             }
@@ -344,27 +461,39 @@ function preencherPeca(cor) {
     }
 }
 
+
+var tela = document.getElementById("tela");
+var GameOver = document.getElementById("Game");
+var scoreGameOver = document.getElementById("scoreGameOver");
+
 function travarPeca(){
+    var contagem = 0;
     for (var i = 0; i < peca.tetraminoAtivo.length; i++) {
         for (var j = 0; j < peca.tetraminoAtivo.length; j++) {
             // pula os quadrados vazios.
-            if (!peca.tetraminoAtivo[i][j]) {
+            if (!peca.tetraminoAtivo[i][j]) { 
                 continue;
             }
 
             //peças que vão ser travadas no topo do tabuleiro (GAME OVER!).
             if (peca.y + i < 0) {
+                pauseAudio("startGame");
                 playAudio("gameOver");
-                alert("GAME OVER!");
                 fimDeJogo = true; // para parar o request animation frame.
+                tela.classList.add("hidde");
+                scoreGameOver.innerHTML = score;
+                GameOver.classList.remove("hidde");
+                estatisticaa.classList.remove("hidde");
                 break;
             }
 
-            //trava as cores da peça nas coordenadas.
+            //trava as cores da peça no tabuleiro nas coordenadas da peça.
             tabuleiro[peca.y+i][peca.x+j] = peca.cor;
+            playAudio("fall");
         }
     }
 
+    //toda vez que uma peça é travada, é necessário checar se um linha foi completada.
     for (var i = 0; i < LINHA; i++) {
         var linhaCheia = true;
 		
@@ -372,27 +501,45 @@ function travarPeca(){
             linhaCheia = linhaCheia && (tabuleiro[i][j] != VAGO);
         }
 		
-        // se a linha estiver completa, a linha é movida para baixo.
+        // se a linha estiver completa, as linhas são movidas para baixo.
         if (linhaCheia) {
-            for (var y = i; y > 1; y--) {
+            for (var y = i; y > 1; y--) { // y = i -> linha que foi completa.
                 for (var j = 0; j < COLUNA; j++) {
-                    tabuleiro[y][j] = tabuleiro[y-1][j];
+                    tabuleiro[y][j] = tabuleiro[y-1][j]; //move para baixo. linha dbaixo == linha dcima.
                 }
             }
 			
+            //cria a primeira linha dnv, já que ela não tinha nada acima.
             for (var j = 0; j < COLUNA; j++) {
                 tabuleiro[0][j] = VAGO;
             }
+            
             playAudio("linhasRemovidas");
-            score += 100*nivel;
+            contagem++;
             linhasRemovidas++;
             linhasRemovidasCaixa.innerHTML = linhasRemovidas;
             if(linhasRemovidas == 10){
                 nivel++;
+                tempo *= 0.8;
+                console.log(tempo);
+                playAudio("levelUp");
                 linhasRemovidas = 0;
+                linhasRemovidasCaixa.innerHTML = linhasRemovidas;
                 nivelCaixa.innerHTML = nivel;
             }
         }
+    }
+    if(contagem == 1){
+        score += 100*nivel;
+    }
+    else if(contagem == 2){
+        score += 300*nivel;
+    }
+    else if(contagem == 3){
+        score += 500*nivel;
+    }
+    else if(contagem == 4){
+        score += 800*nivel;
     }
 	//update no tabuleiro
     desenharTabuleiro();
@@ -403,10 +550,10 @@ function travarPeca(){
 function rodarPeca(){
     var proximoPadrao = peca.tetramino[(peca.tetraminoN + 1) % peca.tetramino.length];
     var recuo = 0;
-    
+
     if (colisao(0, 0, proximoPadrao)) {
         if (peca.x > COLUNA/2) { //a colisão está acontecendo na parede direita.
-            recuo = -1;
+            recuo = -1; //Joga para esquerda.
         } else {
             recuo = 1;
         }
@@ -421,26 +568,66 @@ function rodarPeca(){
     }
 }
 
+function rodarPecaEsquerda(){
+    var proximoPadrao = peca.tetramino[(peca.tetraminoN + 3) % peca.tetramino.length];
+    var recuo = 0;
+
+    if (colisao(0, 0, proximoPadrao)) {
+        if (peca.x > COLUNA/2) { //a colisão está acontecendo na parede direita.
+            recuo = -1;
+        } else {
+            recuo = 1;
+        }
+    }
+    
+    if (!colisao(recuo, 0, proximoPadrao)) {
+        apagarPeca();
+        peca.x += recuo;
+        peca.tetraminoN = (peca.tetraminoN + 3) % peca.tetramino.length;
+        peca.tetraminoAtivo = peca.tetramino[peca.tetraminoN];
+        desenharPeca();
+    }
+}
+
 function controlarPeca(evento){
 	var tecla = evento.keyCode;
-	
-    if (tecla == 37) {
-        moverEsquerda();
-        inicioDescida = Date.now();
-    } else if (tecla == 38 || tecla == 90) {
-        rodarPeca();
-        inicioDescida = Date.now();
-    } else if (tecla == 39) {
-        moverDireita();
-        inicioDescida = Date.now();
-    } else if (tecla == 40) {
-        moverAbaixo();
+	if(!fimDeJogo){
+        if (tecla == 37) {
+            playAudio("move");
+            moverEsquerda();
+            inicioDescida = Date.now();
+        } else if (tecla == 38) {
+            rodarPeca();
+            playAudio("rotate");
+            inicioDescida = Date.now();
+        } else if (tecla == 39) {
+            playAudio("move");
+            moverDireita();
+            inicioDescida = Date.now();
+        } else if (tecla == 40) {
+            playAudio("move");
+            moverAbaixo();
+            score += 1;
+            scoreCaixa.innerHTML = score;
+        }
+        else if(tecla == 32){
+            descerForte();
+        }
+        else if(tecla == 90){
+            rodarPecaEsquerda();
+            playAudio("rotate");
+        }
     }
 }
 
 var linhasRemovida = document.getElementById("linhasremovidas");
 var gameOver = document.getElementById("gameOver");
 var startGame = document.getElementById("startGame");
+var levelUp = document.getElementById("levelUp");
+var fall = document.getElementById("fall");
+var move = document.getElementById("move");
+var rotate = document.getElementById("rotate");
+var Rank = document.getElementById("Rank");
 
 function playAudio(audio) {
     if(audio === "linhasRemovidas"){
@@ -452,4 +639,76 @@ function playAudio(audio) {
     else if(audio == "startGame"){
         startGame.play();
     } 
+    else if(audio == "levelUp"){
+        levelUp.play();
+    }
+    else if(audio == "fall"){
+        fall.play();
+    }
+    else if(audio == "move"){
+        move.play();
+    }
+    else if(audio == "rotate"){
+        rotate.play();
+    }
+}
+
+function pauseAudio(audio){
+    if(audio == "startGame"){
+        startGame.pause();
+    }
+}
+
+//Persistencia de Dados ---> RANK (LocalStorage)
+localStorage.getItem("vetor");
+var vetor = localStorage.getItem("vetor") ?  JSON.parse(localStorage.getItem("vetor")) : [];
+
+var nome1 = document.getElementById("nome1");
+var pt1 = document.getElementById("pt1");
+var nome2 = document.getElementById("nome2");
+var pt2 = document.getElementById("pt2");
+var nome3 = document.getElementById("nome3");
+var pt3 = document.getElementById("pt3");
+var nome3 = document.getElementById("nome3");
+var pt4 = document.getElementById("pt4");
+var nome4 = document.getElementById("nome4");
+var pt5 = document.getElementById("pt5");
+var nome5 = document.getElementById("nome5");
+
+//Bubble Sort para ordenação --> Botão "Enviar" HTML.
+function bubbleSort(items){
+    GameOver.classList.add("hidde");
+    Rank.classList.remove("hidde");
+    var iniciais = document.getElementById("iniciais").value;
+    var pontos = score;
+    items.push({name: iniciais,score: pontos});
+
+    var aux;
+
+    for (var i = 0; i < items.length-1; i++) {
+        for (var j = 0; j < items.length-1-i; j++) {
+            if (parseInt(items[j].score) < parseInt(items[j+1].score)) {
+                aux = items[j];
+                items[j] = items[j+1];
+                items[j+1] = aux;
+            }
+        }
+    }
+
+    localStorage.setItem("vetor",JSON.stringify(items));
+    console.log("storage: ", localStorage.getItem("vetor"));
+    console.log("itens: ",items);
+    
+    //Mostrar o resultado na tela de rank.
+    nome1.innerHTML = items[0].name;
+    pt1.innerHTML = items[0].score;
+    nome2.innerHTML = items[1].name;
+    pt2.innerHTML = items[1].score;
+    nome3.innerHTML = items[2].name;
+    pt3.innerHTML = items[2].score;
+    nome4.innerHTML = items[3].name;
+    pt4.innerHTML = items[3].score;
+    nome5.innerHTML = items[4].name;
+    pt5.innerHTML = items[4].score;
+      
 }
